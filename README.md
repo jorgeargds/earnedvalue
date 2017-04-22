7094ba242d05b5acf48d83d208e50d9e74982cd7
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2017-04-21 19:10:15 -0600

    Matriz de riesgos

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# earnedvalue
-Earned value proyect 
+#Earned Value
 
-https://bootswatch.com/flatly/#progress-bars
+Aplicación que calcula el EV de un proyecto en un determinado momento y que también tiene la capacidad de manejar los riesgos del mismo mediante la matriz de riesgos.
 
-http://fontawesome.io/cheatsheet/
+##Instalacion
+
+```bash
+$ git clone https://github.com/jorgeargds/earnedvalue.git
+
+$ npm install
+
+$ npm build
+
+$ node server.js
+```
+
+##Acerca de
+
+Desarrollado por:
+
+	- Jorge Arguedas
+	- Juan Carlos Sánchez 
+	- María Jesús Gutiérrez
+
+#Librerias 
+
+- https://bootswatch.com/flatly/#progress-bars
+- http://fontawesome.io/cheatsheet/
--- earnedvalue/src/app/riskMatrix.component.ts
+++ earnedvalue/src/app/riskMatrix.component.ts
@@ -2,34 +2,23 @@ import { Component } from '@angular/core'
 import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 
-
 @Component({
   selector: 'riskMatrix',
   templateUrl: 'app/view/riskMatrix.html'
 })
 export class RiskMatrixComponent {
-  // private baseUrl: string = 'http://localhost:8080';
-  // products: any[];
-  // constructor(private http: Http) {
-  //   this.getAllProducts();
-  // }
-  //
-  // getAllProducts() {
-  //   this.http.get(`${this.baseUrl}/getAllProducts`, { headers: this.getHeaders() })
-  //     .map(res => res.text())
-  //     .subscribe(
-  //     data => this.products = JSON.parse(data),
-  //     err => this.logError(err),
-  //   );
-  // }
-  // private getHeaders() {
-  //   let headers = new Headers();
-  //   headers.append('Accept', 'application/json');
-  //
-  //   return headers;
-  // }
-  // logError(err: String) {
-  //   console.error('There was an error: ' + err);
-  // }
 
-}
+  console.console.log("gg");
+  public riesgos: any[];
+
+  constructor(private http: Http) {
+    riesgos.push({"riesgo":"Riesgo #1","proabilidad":70,"impacto":5});
+    riesgos.push({"riesgo":"Riesgo #2","proabilidad":20,"impacto":3});
+    riesgos.push({"riesgo":"Riesgo #3","proabilidad":50,"impacto":1});
+  };
+
+  public btnGetResponse(){
+    console.log('gg');
+  }
+
+};
